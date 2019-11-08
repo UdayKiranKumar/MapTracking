9b1dcbd2982aa37d53d2f14e97873d0d75dbd89d
UdayKiranKumar <udaykirankumar8392@gmail.com>
2019-11-09 00:25:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/maptracking/Modules/DirectionFinderListener.java
@@ -0,0 +1,15 @@
+package com.example.maptracking.Modules;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Mai Thanh Hiep on 4/3/2016.
+ */
+public interface DirectionFinderListener {
+    void onDirectionFinderStart();
+    void onDirectionFinderSuccess(List<Route> route);
+}
